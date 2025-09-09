85c488c7f19f0e8e382a8c112746c34c2983556f
Saathwika Dhenuvakonda <saathwikadhenuvakonda@gmail.com>
2025-09-09 12:06:51 +0530

    Project Setup with Next.js and Tailwind.

---
+++ tessera/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>Welcome to Tessera!</h1>
+    </main>
+  );
+}
